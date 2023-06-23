1cfb05ddce1cc43379b8d3dd1fd7c577bc0afde5
唐王瑶 <tangwangyao@hualala.com>
2023-06-23 23:19:54 +0800

    feat:  注册类库挂载类库

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,24 @@
+<!--
+ * @Author: callus
+ * @Date: 2022-07-14 11:25:04
+ * @LastEditors: callus
+ * @Description: some description
+ * @FilePath: /vite-mother-day1/src/App.vue
+-->
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-    这是新的提交
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Header />
+  <router-view />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style>
+ul,
+li {
+  list-style: none;
+  margin: 0;
+  padding: 0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,28 @@
 /*
  * @Author: callus
- * @Date: 2023-06-20 23:42:40
+ * @Date: 2022-07-14 10:21:34
  * @LastEditors: callus
  * @Description: some description
  * @FilePath: /vite-mother-day1/src/main.js
  */
-import { createApp } from 'vue'
-import './style.css'
+import { createApp } from "vue";
+import { setupWorker } from "msw";
+import Antd from 'ant-design-vue'
 import App from './App.vue'
+import router from './routers/index.js'
+import mock from "./mocks";
+import 'ant-design-vue/dist/antd.less'
+import "normalize.css";
 // if (process.env.NODE_ENV === 'development') {
-  //   const { worker } = require('./mocks/browser')
-  //   worker.start()
-  // }
+//     const { worker } = require('./mocks/browser')
+//     worker.start()
+//   }
 
-createApp(App).mount('#app')
+const worker = setupWorker(...mock);
+
+worker.start({});
+
+const app = createApp(App)
+app.use(Antd)
+app.use(router)
+app.mount("#app");
